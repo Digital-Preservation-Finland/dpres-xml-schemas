3f1222f68ce0358723ba975b8bf3d2394fd010c3
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-05-22 21:40:31 +0300

    KDKPAS-1441 Updated DDI 2.0 schema in the catalog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from version import get_version
 def main():
     """Install dpres-xml-schemas Python libraries"""
     setup(
-        name='dpres-xml-schemas',
+        name='dpres_xml_schemas',
         packages=find_packages(exclude=['tests', 'tests.*']),
         version=get_version())
 
