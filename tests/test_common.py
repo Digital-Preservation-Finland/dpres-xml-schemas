2d4be378257b5f57a720fdce8dd48cb388ddc456
Vili Oja <vili.oja@csc.fi>
2022-01-07 16:27:32 +0200

    KDKPAS-2737 Make test_common.py use lxml

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,7 +2,7 @@
 
 .. seealso:: common.py
 """
-import xml.etree.ElementTree as ET
+import lxml.etree as ET
 from tests.common import add_containers, parse_xml_string
 
 
@@ -12,6 +12,6 @@ def test_container():
     (_, root) = parse_xml_string(
         '<mets:foo xmlns:mets="http://www.loc.gov/METS/"/>')
     (_, root) = add_containers(root, 'mets:mets/mets:amdSec')
-    xml = '<ns0:mets xmlns:ns0="http://www.loc.gov/METS/">' \
-        '<ns0:amdSec><ns0:foo /></ns0:amdSec></ns0:mets>'
+    xml = b'<mets:mets xmlns:mets="http://www.loc.gov/METS/">' \
+        b'<mets:amdSec><mets:foo/></mets:amdSec></mets:mets>'
     assert ET.tostring(root) == xml
